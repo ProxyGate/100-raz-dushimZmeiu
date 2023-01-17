fe7dd00404caeb1d16414690841afdeaa50d424e
FlyPirate <83443746+ProxyGate@users.noreply.github.com>
2023-01-17 23:00:28 +0000

    2.1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Cуммируем первые две цифры двухзначного числа
 # 🚨 Don't change the code below 👇
 two_digit_number = input("Type a two digit number: ")
 # 🚨 Don't change the code above 👆
@@ -7,6 +8,20 @@ print(int(two_digit_number[0]) + int(two_digit_number[1]))
 ####################################
 #Write your code below this line 👇
 
+# =============================================================================================================================================
+
+# Рассчет индекса массы тела
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+BMI = int(weight) / float(height) ** 2
+print("Your BMI = " + str(int(BMI)))
+
+# =============================================================================================================================================
 
 
 
