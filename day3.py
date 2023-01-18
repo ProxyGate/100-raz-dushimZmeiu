5460683651e487e67ec193f3c12f338dd9a78ace
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2023-01-18 22:45:49 +0300

    Update day3.py
    3.2

--- day3.py
+++ day3.py
@@ -9,3 +9,26 @@ else:
   print ("Твое число четное")
 
 #===================================================================================================================================================
+
+# Рассчет индекса массы тела
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+BMI = int(weight) / float(height) ** 2
+
+if BMI < 18.5:
+  print("Your BMI = " + str(BMI) + ", you are underweight")
+elif BMI < 25:
+  print("Your BMI = " + str(BMI) + ", you are normal weight")
+elif BMI < 30:
+  print("Your BMI = " + str(BMI) + ", you are overweight")
+elif BMI < 35:
+  print("Your BMI = " + str(BMI) + ", you are obese")
+else:
+  print("Your BMI = " + str(BMI) + ", you are clinically obese")
+
+# =============================================================================================================================================
