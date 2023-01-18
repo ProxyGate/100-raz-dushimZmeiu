9aaf2cc9a5e66895ac0c1cfdba528bb36eee9901
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2023-01-19 00:42:27 +0300

    Update day3.py
    3.3

--- day3.py
+++ day3.py
@@ -54,3 +54,36 @@ else:
       print ("Годик то високосный")
         
 # =============================================================================================================================================
+
+#Выбор пиццы
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bill = 0
+
+if size == "s" or size == "S":
+  bill = 15
+  if add_pepperoni == "y" or add_pepperoni == "Y":
+    bill += 2
+if size == "m" or size == "M":
+  bill = 20
+  if add_pepperoni == "y" or add_pepperoni == "Y":
+    bill += 3
+if size == "l" or size == "L":
+  bill = 25
+  if add_pepperoni == "y" or add_pepperoni == "Y":
+    bill += 3
+
+if extra_cheese == "y" or extra_cheese == "Y":
+    bill += 1
+
+print (f"Your total bill = {bill}")
+
+# =============================================================================================================================================
+
