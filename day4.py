eba7d86c6e0db8e4743fc35fe208ff54d0c3f910
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2023-01-20 00:31:30 +0300

    Update day4.py
    4.3

--- day4.py
+++ day4.py
@@ -28,4 +28,39 @@ print(f"Za s4et platit {names[kto_na_samom_dele]}")
 
 #=====================================================================================================================================================================
 
+#Карта сокровищ
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
 
+#Write your code below this row 👇
+stolbets = int(position[0]) - 1
+stroka = int(position[1]) - 1
+
+if stroka == 0:
+  row1.insert(stolbets, "x ") 
+  row1.pop(stolbets+1)
+elif stroka == 1:
+  row2.insert(stolbets, "x ") 
+  row2.pop(stolbets+1)
+elif stroka == 2:
+  row3.insert(stolbets, "x ") 
+  row3.pop(stolbets+1)
+else:
+  print("Incorrect number")
+
+# row = locals()["row" + position[1]]     #определяем строку
+# row[int(position[0])-1] = 'X'         #заменяем позицию в строке
+# # print (row[int(position[0])-1])       #определяем и печатаем позицию в строке 
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
+#=====================================================================================================================================================================
