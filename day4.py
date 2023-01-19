bd82b104e3bdc8ea7fb35be36f35f9cfe357aef6
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2023-01-19 22:19:26 +0300

    Update day4.py
    4.2

--- day4.py
+++ day4.py
@@ -11,3 +11,21 @@ elif monetka == 2:
   print("У вас решка")
 
 #=====================================================================================================================================================================
+
+#Кто из компании платит за счет
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+import random
+number_of_people = len(names)
+kto = random.randint(1,number_of_people)
+kto_na_samom_dele = kto - 1
+print(f"Za s4et platit {names[kto_na_samom_dele]}")
+
+#=====================================================================================================================================================================
+
+
