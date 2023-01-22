6149fff699db93ace63f4e4df440190c3063e1a0
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2023-01-23 00:32:30 +0300

    Update day5.py
    5.5

--- day5.py
+++ day5.py
@@ -58,3 +58,95 @@ for a in range(1,101):
   print (a)
 
 #=======================================================================================================================================================================
+
+#Генератор паролей (рандомный(совсем))
+#Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+a = 0
+b = []
+c = ""
+d = ""
+for a in range(0, nr_letters): 
+  r_letter = random.choice(letters)
+  b += r_letter
+for a in range(0, nr_numbers): 
+  r_number = random.choice(numbers)
+  b += r_number
+for a in range(0, nr_symbols): 
+  r_symbol = random.choice(symbols)
+  b += r_symbol
+
+for a in range(0, len(b)):
+  r_string = random.choice(b)
+  d += r_string
+  b.remove(r_string)
+
+c = ''.join(map(str, d))
+print(f"Your password: \n{c}\n(Dont trust password generators st00pid!)")
+
+#=======================================================================================================================================================================
+#EBANINA
+# #Password Generator Project
+# import random
+
+# letters = [
+#     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+#     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+#     'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+#     'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+# ]
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# #Eazy Level - Order not randomised:
+# #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+# #Hard Level - Order of characters randomised:
+# #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+# passw = str()
+# pass_len = nr_letters + nr_symbols + nr_numbers
+# lc = 0
+# sc = 0
+# nc = 0
+
+# currlen = 0
+# for i in  range(0, pass_len):
+#     x = random.randint(1, 3)
+#     if ((x == 1) or ((nc == nr_numbers) and (sc == nr_symbols))) and (lc < nr_letters):
+#       l = random.randint(0, len(letters) - 1)
+#       passw = passw + letters[l]
+#       print(f"x - {x}; lc - {lc}; l - {l}")
+#       lc += 1
+#     elif ((x <= 2) or (sc == nr_symbols)) and (nc < nr_numbers):
+#       n = random.randint(0, len(numbers) - 1)
+#       passw = passw + numbers[n]
+#       print(f"x - {x}; nc - {nc}; n - {n}")
+#       nc += 1
+#     elif (x <= 3) and (sc < nr_symbols):
+#       s = random.randint(0, len(symbols) - 1)
+#       passw = passw + symbols[s]
+#       print(f"x - {x}; sc - {sc}; s - {s}")
+#       sc += 1
+#    # currlen = lc + nc + sc
+
+
+# print(f"len - {pass_len}; len(p) - {len(passw)}")
+# print(f"Parol - {passw}") 
+
+#=======================================================================================================================================================================
+
