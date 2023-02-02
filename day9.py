4102bd2d969d20ea2ca3e2f64184f74aeea95bf7
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2023-02-02 03:21:08 +0300

    Update day9.py
    9.4

--- day9.py
+++ day9.py
@@ -24,6 +24,39 @@ for i in student_scores:
 print(student_grades)
 
 #=====================================================================================================================================================================
+#vlojennie slovari/spiski
+cities_visited = {}
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"]},
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+print(travel_log)
+#=====================================================================================================================================================================
+#travel log
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+
+def add_new_country (a, b, c):
+  travel_log.append({
+  "country": a,
+  "visits": b,
+  "cities": c  
+  })
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+#=====================================================================================================================================================================
+
 
 
 
