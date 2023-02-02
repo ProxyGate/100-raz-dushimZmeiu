189661385a467fdc9ffd3110a526a73934b98208
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2023-02-02 03:22:22 +0300

    Update day9.py
    9.5 fin

--- day9.py
+++ day9.py
@@ -57,6 +57,36 @@ add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
 #=====================================================================================================================================================================
 
+#TENEVOI AUCTION
+
+from replit import clear
+from art import logo
+print(logo)
+print ("Dobro pojalovat' v sekretniy skritiy auction, ne tolko lish vse mogut suda popast")
+
+clown_list = {}
+def u_kogo_bolshe_biba (a, b):
+  clown_list[a] = b
+  
+ludi = "da"
+while ludi == "da":
+  name = input("Nazovi svoe imya, stalker ")
+  dengi = int(input("Jopu stavish? ili chto tam u tebya? $"))
+  ludi = input("Nu che, s toboi eshe hlop4iki est'? Bistro - 'da', 'net'?\n")
+  u_kogo_bolshe_biba(name, dengi)
+  clear()
+  
+c = 0
+d = ""  
+for i in clown_list:
+  if clown_list[i] > c:
+    c = clown_list[i]
+    d = i
+print(f"Samaya bolshaya biba - {c}cm u {d}")
+print(clown_list)
+
+#=====================================================================================================================================================================
+
 
 
 
