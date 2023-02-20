d3f3ba842f2f0e1705074d666a433eceb20357ff
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2023-02-21 01:07:15 +0300

    Create day19 (turtle race).py

---
+++ day19 (turtle race).py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+donatello = Turtle()
+mikelangello = Turtle()
+
+screen = Screen()
+
+# ##risovalka
+# def forward():
+#     donatello.forward(50)
+#
+# def backwards():
+#     donatello.backward(50)
+#
+# def turn_left():
+#     donatello.left(10
+#
+# def turn_right():
+#     donatello.right(10)
+#
+# screen.listen()
+# screen.onkey(forward, "w")
+# screen.onkey(backwards, "s")
+# screen.onkey(turn_left, "a")
+# screen.onkey(turn_right, "d")
+
+
+
+screen.exitonclick()
